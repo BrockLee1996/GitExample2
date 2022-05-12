91b0e1c4e162e1e798c45cdc4a9a6b67d3b49d60
Andrew Scheer <apscheer@protonmail.com>
2022-05-12 08:41:31 -0600

    Init Commit

---
+++ gui.py
@@ -0,0 +1,9 @@
+import os 
+import sys
+def run_numbers(num1,num2):
+    print(num1-num2)
+def print_name(name):
+    print("This is your name:"+name)
+run_numbers(5,5)
+print(os.curdir)
+run_numbers(5,5)
